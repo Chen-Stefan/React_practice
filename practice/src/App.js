8f4c7679247f2f6c518ba78f61e7fbc0e7f95f21
Stefan Chen <zchen158@my.bcit.ca>
2022-06-12 00:08:38 -0700

    Create first functional Component

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+// Greet后面的.js在import path里可以省略
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet/>
     </div>
   );
 }
