be7a6e190805e8127a2caac02c06158f68b87b45
Stefan Chen <zchen158@my.bcit.ca>
2022-06-20 22:42:22 -0700

    Use index as key prop

--- practice/src/components/NameList.js
+++ practice/src/components/NameList.js
@@ -1,26 +1,30 @@
 import React from "react";
-import Person
- from "./Person";
+import Person from "./Person";
+
 function NameList() {
+  const names = ['Bruce', 'Clark', 'Diana']
   const persons = [
     {
       id: 1,
-      name: 'Bruce',
-      age: 30
+      name: "Bruce",
+      age: 30,
     },
     {
       id: 2,
-      name: 'Clark',
-      age: 25
+      name: "Clark",
+      age: 25,
     },
     {
       id: 3,
-      name: 'Diana',
-      age: 28
-    }
-  ]
-  const personList = persons.map(person => <Person key={person.id} person = {person} />);
-  return <div>{personList}</div>;
+      name: "Diana",
+      age: 28,
+    },
+  ];
+  // const personList = persons.map((person) => (
+  //   <Person key={person.id} person={person} />
+  // ));
+  const nameList = names.map((name, index) => <h2 key = {index} >{name}</h2>)
+  return <div>{nameList}</div>;
 }
 
 export default NameList;
