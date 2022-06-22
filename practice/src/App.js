a2183fa559a99bfcfac67f6ba71134f3fc73a2c1
Stefan Chen <zchen158@my.bcit.ca>
2022-06-21 22:03:27 -0700

    Style using inline styling

--- practice/src/App.js
+++ practice/src/App.js
@@ -15,12 +15,13 @@ import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import Person from './components/Person';
 import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Stylesheet primary={true} />
+        <Inline />
     </div>
     );
   }
---
+++ practice/src/components/Inline.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const heading = {
+  fontSize: "72px",
+  color: "green",
+};
+
+function Inline() {
+  return (
+    <div>
+      <h1 style={heading}>Inline</h1>
+    </div>
+  );
+}
+
+export default Inline;
