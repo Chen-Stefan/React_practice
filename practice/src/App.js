6f112f5027ba3bf5a4ae2281dd6765fdb63ee078
Stefan Chen <zchen158@my.bcit.ca>
2022-06-21 22:00:18 -0700

    Conditional style and multiple className using template literal

--- practice/src/App.js
+++ practice/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Stylesheet />
+        <Stylesheet primary={true} />
     </div>
     );
   }
--- practice/src/components/Stylesheet.js
+++ practice/src/components/Stylesheet.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './myStyles.css'
 
-function Stylesheet() {
+function Stylesheet(props) {
+  let className = props.primary ? 'primary' : ''
   return (
-    <div><h1 className='primary'>Stylesheets</h1></div>
+    <div><h1 className={`${className} font-xl`}>Stylesheets</h1></div>
   )
 }
 
